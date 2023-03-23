75841b7dc5a02babc57032849233636c692b7bbb
Lanng21 <mailanbiun@gmail.com>
2023-03-23 22:52:51 +0700

    feat: Create UI table show list customer

--- src/views/components/index.tsx
+++ src/views/components/index.tsx
@@ -1,10 +1,56 @@
 import React from 'react'
-import EmptyCustomer from '@/views/customer/components/Empty'
+import Table from './common/Table'
+import { ColumnsType } from 'antd/es/table'
+import { data } from '../Constants/customer'
 
 const Components = () => {
+  const columns: ColumnsType<object> = [
+    {
+      key: 'id',
+      title: 'STT',
+      dataIndex: 'id',
+      width: '5%',
+    },
+    {
+      key: 'name',
+      title: 'Tên đầy đủ',
+      dataIndex: 'name',
+      width: '25%',
+    },
+    {
+      key: 'phoneNumber',
+      title: 'Số điện thoại',
+      dataIndex: 'phoneNumber',
+      width: '15%',
+    },
+    {
+      key: 'staff',
+      title: 'Nhân viên phụ trách',
+      dataIndex: 'staff',
+      width: '20%',
+    },
+    {
+      key: 'demand',
+      title: 'Nhu cầu',
+      dataIndex: 'demand',
+      width: '15%',
+    },
+    {
+      key: 'createdAt',
+      title: 'Ngày tạo',
+      dataIndex: 'createdAt',
+      width: '15%',
+    },
+    {
+      key: 'nullColums',
+      title: '',
+      dataIndex: 'nullColums',
+      width: '5%',
+    },
+  ]
   return (
     <div>
-      <EmptyCustomer />
+      <Table columns={columns} dataSource={data} />
     </div>
   )
 }
---
+++ src/views/types/table/index.ts
@@ -0,0 +1,10 @@
+export interface Idata {
+  id: number;
+  name: string;
+  avatar?: string;
+  phoneNumber?: string;
+  staff?: string;
+  demand?: string;
+  createdAt?: string;
+  nullColums?: string
+}
