d4e9f88451f41b621bc8aecb4bf2095310550a77
xuanquocc <quocntx.21it@vku.udn.vn>
2023-03-26 20:15:49 +0700

    add icon for popup over

---
+++ src/views/icons/Notes.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Notes: React.FC = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        d="M6.66602 11.668L13.3327 11.668"
+        stroke="#787E87"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.66602 8.33398L8.33268 8.33398"
+        stroke="#787E87"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.66602 15L9.99935 15"
+        stroke="#787E87"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.33301 2.50065H7.33301C5.12387 2.50065 3.33301 4.29151 3.33301 6.50065V14.334C3.33301 16.5431 5.12387 18.334 7.33301 18.334H12.6663C14.8755 18.334 16.6663 16.5431 16.6663 14.334V6.50065C16.6663 4.29151 14.8755 2.50065 12.6663 2.50065H12.083M8.33301 2.50065V0.833984M8.33301 2.50065V4.16732"
+        stroke="#787E87"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Notes;
