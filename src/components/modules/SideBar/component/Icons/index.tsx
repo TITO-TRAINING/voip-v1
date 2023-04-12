664a6e2977dee9ed03bd990c686efe0615edfe17
Lanng21 <mailanbiun@gmail.com>
2023-04-12 15:50:16 +0700

    Update: path of icons

--- src/components/modules/SideBar/component/Icons/index.tsx
+++ src/components/modules/SideBar/component/Icons/index.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent as ClientCustomer } from '@/assets/icons/ClientCustomer.svg'
 import { ReactComponent as Ticket } from '@/assets/icons/Ticket.svg'
 import { ReactComponent as Communicate } from '@/assets/icons/Communicate.svg'
-import { ReactComponent as Integration } from '@/assets/icons/Ticket.svg'
+import { ReactComponent as Integration } from '@/assets/icons/Integration.svg'
 import { ReactComponent as Call } from '@/assets/icons/Call.svg'
 import { ReactComponent as Statistics } from '@/assets/icons/Statistics.svg'
 import { ReactComponent as Employee } from '@/assets/icons/Employee.svg'
