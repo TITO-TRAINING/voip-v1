d446e3b05c454acb9c34f201ec6b092862d2e36b
Lanng21 <mailanbiun@gmail.com>
2023-04-13 11:27:14 +0700

    feat: config routes for side bar

---
+++ src/constants/path.ts
@@ -0,0 +1,10 @@
+export enum PATH {
+  STATISTICS = '/statistics',
+  TICKET = '/tickets',
+  CUSTOMER = '/customers',
+  CONTACTS = '/contacts',
+  CALL_HISTORY = '/switchboard',
+  STAFFS = '/staffs',
+  SETTINGS = '/settings',
+  INTEGRATE = '/integrate',
+}
